c4456bb5271941e3efe99cec9486dc7f141b77d3
Vladislav Shyrokiy <shyrokij009@gmail.com>
2023-02-07 10:59:55 +0200

    md-files-new

--- js-info/02-operators-js/operators.js
+++
@@ -1,9 +0,0 @@
-// Приоретет операторов //
-
-// const fullAge = 26
-// const birthYear = 1996
-// const currentYear = 2022
-
-// const isFullAge = currentYear - birthYear >= fullAge
-
-// console.log(isFullAge)
